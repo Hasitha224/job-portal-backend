936151899791d6d019917882b61f092cc8c013a3
Hasitha224 <hasithakalana100@gmail.com>
2023-12-08 19:48:59 +0530

    auth Middleware added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const verifyToken = require('../Middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/signup',userController.register);
 router.post('/login',userController.login);
-
+router.get('/uservacancy',verifyToken);
+router.get('/apply',verifyToken);
+router.get('/status',verifyToken);
 module.exports = router;
